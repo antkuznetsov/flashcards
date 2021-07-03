e55a7cfeb00e053963f10a13ac8f7f852fa42e87
Anton Kuznetsov <antkuznetsov@yandex.ru>
2021-07-03 14:53:52 +0200

    Implemented Stage 3 - created a new card for the program to play with player

--- src/flashcards/Main.java
+++ src/flashcards/Main.java
@@ -1,7 +1,30 @@
 package flashcards;
 
+import java.util.Scanner;
+
 public class Main {
+    final static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.print("Input the number of cards:\n");
+        int numberOfCards  = Integer.parseInt(scanner.nextLine());
+
+        String[] terms = new String[numberOfCards];
+        String[] definitions = new String[numberOfCards];
+
+        for (int i = 0; i < numberOfCards; i++) {
+            System.out.printf("The card #%d:\n", i + 1);
+            String term = scanner.nextLine();
+            System.out.printf("The definition of the card #%d:\n", i + 1);
+            String definition  = scanner.nextLine();
+            terms[i] = term.trim();
+            definitions[i] = definition.trim();
+        }
+
+        for (int i = 0; i < numberOfCards; i++) {
+            System.out.printf("Print the definition of \"%s\":\n", terms[i]);
+            String userAnswer  = scanner.next();
+            String rightAnswer = definitions[i];
+            System.out.println(rightAnswer.equals(userAnswer) ? "Correct!" : String.format("Wrong. The right answer is \"%s\".", rightAnswer));
+        }
     }
 }
