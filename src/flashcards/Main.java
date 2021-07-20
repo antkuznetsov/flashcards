7234f05fc4ab1d7dacdfe9ce43bc83a1f7c14b9a
Anton Kuznetsov <antkuznetsov@yandex.ru>
2021-07-20 18:08:38 +0200

    Implemented Stage 5

--- src/flashcards/Main.java
+++ src/flashcards/Main.java
@@ -1,6 +1,8 @@
 package flashcards;
 
+import java.io.*;
 import java.util.LinkedHashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -8,49 +10,133 @@ public class Main {
     private static final Scanner scanner = new Scanner(System.in);
     private static final Map<String, String> data = new LinkedHashMap<>();
 
-    public static void main(String[] args) {
-        fillData();
-        learnTerms();
+    private enum Action {
+        ADD,
+        REMOVE,
+        IMPORT,
+        EXPORT,
+        ASK,
+        EXIT,
+        UNKNOWN,
     }
 
-    private static void fillData() {
-        System.out.print("Input the number of cards:\n");
-        int numberOfCards = Integer.parseInt(scanner.nextLine());
-        for (int i = 0; i < numberOfCards; i++) {
-            boolean repeat;
-            String term;
-            String definition;
-
-            System.out.printf("Card #%d:\n", i + 1);
-            do {
-                term = scanner.nextLine();
-
-                if (data.containsKey(term)) {
-                    System.out.printf("The term \"%s\" already exists. Try again:%n", term);
-                    repeat = true;
-                } else {
-                    repeat = false;
-                }
-            } while (repeat);
-
-            System.out.printf("The definition for card #%d:\n", i + 1);
-            do {
-                definition = scanner.nextLine();
-
-                if (data.containsValue(definition)) {
-                    System.out.printf("The definition \"%s\" already exists. Try again:%n", definition);
-                    repeat = true;
-                } else {
-                    repeat = false;
-                }
-            } while (repeat);
-
-            data.put(term, definition);
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Input the action (add, remove, import, export, ask, exit):");
+            Action action = parseAction(scanner.nextLine());
+            performAction(action);
         }
     }
 
-    private static void learnTerms() {
+    private static Action parseAction(String action) {
+        Action currentAction;
+        try {
+            currentAction = Action.valueOf(action.trim().toUpperCase(Locale.ROOT));
+        } catch (IllegalArgumentException e) {
+            currentAction = Action.UNKNOWN;
+        }
+        return currentAction;
+    }
+
+    private static void performAction(Action action) {
+        switch (action) {
+            case ADD:
+                add();
+                break;
+            case REMOVE:
+                remove();
+                break;
+            case IMPORT:
+                importData();
+                break;
+            case EXPORT:
+                exportData();
+                break;
+            case ASK:
+                ask();
+                break;
+            case EXIT:
+                exit();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private static void add() {
+        System.out.println("The card:");
+        String term = scanner.nextLine();
+        if (data.containsKey(term)) {
+            System.out.printf("The card \"%s\" already exists.%n", term);
+            return;
+        }
+
+        System.out.println("The definition of the card:");
+        String definition = scanner.nextLine();
+        if (data.containsValue(definition)) {
+            System.out.printf("The definition \"%s\" already exists. Try again:%n", definition);
+            return;
+        }
+
+        data.put(term, definition);
+
+        System.out.printf("The pair (\"%s\":\"%s\") has been added.%n", term, definition);
+    }
+
+    private static void remove() {
+        System.out.println("Which card?");
+        String term = scanner.nextLine();
+        if (data.containsKey(term)) {
+            data.remove(term);
+            System.out.println("The card has been removed.");
+        } else {
+            System.out.printf("Can't remove \"%s\": there is no such card.%n", term);
+        }
+    }
+
+    private static void importData() {
+        System.out.println("File name:");
+        String fileName = scanner.nextLine();
+        int counter = 0;
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            while (scanner.hasNext()) {
+                String[] entity = scanner.nextLine().split(":");
+                data.put(entity[0], entity[1]);
+                counter++;
+            }
+            System.out.printf("%d cards have been loaded.%n", counter);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        }
+    }
+
+    private static void exportData() {
+        System.out.println("File name:");
+        String fileName = scanner.nextLine();
+        File file = new File(fileName);
+
+        try (PrintWriter writer = new PrintWriter(file)) {
+            for (var entry : data.entrySet()) {
+                writer.printf("%s:%s%n", entry.getKey(), entry.getValue());
+            }
+        } catch (IOException e) {
+            System.out.printf("An exception occurs %s", e.getMessage());
+        }
+        System.out.printf("%d cards have been saved.%n", data.size());
+    }
+
+    private static void ask() {
+        System.out.println("How many times to ask?");
+        int count;
+        try {
+            count = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            count = 0;
+        }
         for (var entry : data.entrySet()) {
+            if (count == 0) {
+                break;
+            }
             System.out.printf("Print the definition of \"%s\":\n", entry.getKey());
             String userAnswer = scanner.nextLine();
             String rightAnswer = entry.getValue();
@@ -69,6 +155,12 @@ public class Main {
             } else {
                 System.out.printf("Wrong. The right answer is \"%s\".%n", rightAnswer);
             }
+            count--;
         }
+    }
+
+    private static void exit() {
+        System.out.println("Bye bye!");
+        System.exit(0);
     }
 }
