4aa9a70480220e4c7ac326ba23be4ed35e9bf033
Anton Kuznetsov <antkuznetsov@yandex.ru>
2021-07-20 11:18:23 +0200

    Implemented Stage 4

--- src/flashcards/Main.java
+++ src/flashcards/Main.java
@@ -1,30 +1,74 @@
 package flashcards;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    final static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final Map<String, String> data = new LinkedHashMap<>();
+
     public static void main(String[] args) {
+        fillData();
+        learnTerms();
+    }
+
+    private static void fillData() {
         System.out.print("Input the number of cards:\n");
-        int numberOfCards  = Integer.parseInt(scanner.nextLine());
-
-        String[] terms = new String[numberOfCards];
-        String[] definitions = new String[numberOfCards];
-
+        int numberOfCards = Integer.parseInt(scanner.nextLine());
         for (int i = 0; i < numberOfCards; i++) {
-            System.out.printf("The card #%d:\n", i + 1);
-            String term = scanner.nextLine();
-            System.out.printf("The definition of the card #%d:\n", i + 1);
-            String definition  = scanner.nextLine();
-            terms[i] = term.trim();
-            definitions[i] = definition.trim();
+            boolean repeat;
+            String term;
+            String definition;
+
+            System.out.printf("Card #%d:\n", i + 1);
+            do {
+                term = scanner.nextLine();
+
+                if (data.containsKey(term)) {
+                    System.out.printf("The term \"%s\" already exists. Try again:%n", term);
+                    repeat = true;
+                } else {
+                    repeat = false;
+                }
+            } while (repeat);
+
+            System.out.printf("The definition for card #%d:\n", i + 1);
+            do {
+                definition = scanner.nextLine();
+
+                if (data.containsValue(definition)) {
+                    System.out.printf("The definition \"%s\" already exists. Try again:%n", definition);
+                    repeat = true;
+                } else {
+                    repeat = false;
+                }
+            } while (repeat);
+
+            data.put(term, definition);
         }
+    }
 
-        for (int i = 0; i < numberOfCards; i++) {
-            System.out.printf("Print the definition of \"%s\":\n", terms[i]);
-            String userAnswer  = scanner.next();
-            String rightAnswer = definitions[i];
-            System.out.println(rightAnswer.equals(userAnswer) ? "Correct!" : String.format("Wrong. The right answer is \"%s\".", rightAnswer));
+    private static void learnTerms() {
+        for (var entry : data.entrySet()) {
+            System.out.printf("Print the definition of \"%s\":\n", entry.getKey());
+            String userAnswer = scanner.nextLine();
+            String rightAnswer = entry.getValue();
+
+            if (rightAnswer.equals(userAnswer)) {
+                System.out.println("Correct!");
+            } else if (data.containsValue(userAnswer)) {
+                String rightTerm = "";
+                for (var el : data.entrySet()) {
+                    if (userAnswer.equals(el.getValue())) {
+                        rightTerm = el.getKey();
+                        break;
+                    }
+                }
+                System.out.printf("Wrong. The right answer is \"%s\", but your definition is correct for \"%s\".%n", rightAnswer, rightTerm);
+            } else {
+                System.out.printf("Wrong. The right answer is \"%s\".%n", rightAnswer);
+            }
         }
     }
 }
