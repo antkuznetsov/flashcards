de01e0f61f84a33534bb6052ce20bbb2db2d1390
Anton Kuznetsov <antkuznetsov@yandex.ru>
2021-07-21 23:57:45 +0200

    Implemented Stage 6

--- src/flashcards/Main.java
+++ src/flashcards/Main.java
@@ -1,41 +1,49 @@
 package flashcards;
 
 import java.io.*;
-import java.util.LinkedHashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
     private static final Map<String, String> data = new LinkedHashMap<>();
+    private static final Map<String, Integer> statistics = new HashMap<>();
 
     private enum Action {
-        ADD,
-        REMOVE,
-        IMPORT,
-        EXPORT,
-        ASK,
-        EXIT,
-        UNKNOWN,
+        ADD("add"),
+        REMOVE("remove"),
+        IMPORT("import"),
+        EXPORT("export"),
+        ASK("ask"),
+        EXIT("exit"),
+        LOG("log"),
+        HARDEST_CARD("hardest card"),
+        RESET_STATS("reset stats"),
+        UNKNOWN("unknown"),
+        ;
+
+        String value;
+
+        Action(String value) {
+            this.value = value;
+        }
+
+        public static Action getByKeyword(String keyword) {
+            for (Action action : Action.values()) {
+                if (action.value.equals(keyword)) {
+                    return action;
+                }
+            }
+            return UNKNOWN;
+        }
     }
 
     public static void main(String[] args) {
         while (true) {
-            System.out.println("Input the action (add, remove, import, export, ask, exit):");
-            Action action = parseAction(scanner.nextLine());
+            System.out.println("Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):");
+            Action action = Action.getByKeyword(scanner.nextLine());
             performAction(action);
         }
-    }
-
-    private static Action parseAction(String action) {
-        Action currentAction;
-        try {
-            currentAction = Action.valueOf(action.trim().toUpperCase(Locale.ROOT));
-        } catch (IllegalArgumentException e) {
-            currentAction = Action.UNKNOWN;
-        }
-        return currentAction;
     }
 
     private static void performAction(Action action) {
@@ -57,6 +65,15 @@ public class Main {
                 break;
             case EXIT:
                 exit();
+                break;
+            case LOG:
+                log();
+                break;
+            case HARDEST_CARD:
+                hardestCard();
+                break;
+            case RESET_STATS:
+                resetStats();
                 break;
             default:
                 break;
@@ -100,8 +117,9 @@ public class Main {
         int counter = 0;
         try (Scanner scanner = new Scanner(new File(fileName))) {
             while (scanner.hasNext()) {
-                String[] entity = scanner.nextLine().split(":");
+                String[] entity = scanner.nextLine().split(";");
                 data.put(entity[0], entity[1]);
+                statistics.put(entity[0], Integer.parseInt(entity[2]));
                 counter++;
             }
             System.out.printf("%d cards have been loaded.%n", counter);
@@ -117,7 +135,7 @@ public class Main {
 
         try (PrintWriter writer = new PrintWriter(file)) {
             for (var entry : data.entrySet()) {
-                writer.printf("%s:%s%n", entry.getKey(), entry.getValue());
+                writer.printf("%s;%s;%d%n", entry.getKey(), entry.getValue(), statistics.getOrDefault(entry.getKey(), 0));
             }
         } catch (IOException e) {
             System.out.printf("An exception occurs %s", e.getMessage());
@@ -137,30 +155,91 @@ public class Main {
             if (count == 0) {
                 break;
             }
-            System.out.printf("Print the definition of \"%s\":\n", entry.getKey());
+            String term = entry.getKey();
+            System.out.printf("Print the definition of \"%s\":\n", term);
             String userAnswer = scanner.nextLine();
             String rightAnswer = entry.getValue();
 
             if (rightAnswer.equals(userAnswer)) {
                 System.out.println("Correct!");
-            } else if (data.containsValue(userAnswer)) {
-                String rightTerm = "";
-                for (var el : data.entrySet()) {
-                    if (userAnswer.equals(el.getValue())) {
-                        rightTerm = el.getKey();
-                        break;
-                    }
-                }
-                System.out.printf("Wrong. The right answer is \"%s\", but your definition is correct for \"%s\".%n", rightAnswer, rightTerm);
             } else {
-                System.out.printf("Wrong. The right answer is \"%s\".%n", rightAnswer);
+                if (data.containsValue(userAnswer)) {
+                    String rightTerm = "";
+                    for (var el : data.entrySet()) {
+                        if (userAnswer.equals(el.getValue())) {
+                            rightTerm = el.getKey();
+                            break;
+                        }
+                    }
+                    System.out.printf("Wrong. The right answer is \"%s\", but your definition is correct for \"%s\".%n", rightAnswer, rightTerm);
+                } else {
+                    System.out.printf("Wrong. The right answer is \"%s\".%n", rightAnswer);
+                }
+                updateStatistics(term);
             }
             count--;
+        }
+    }
+
+    private static void updateStatistics(String term) {
+        if (statistics.containsKey(term)) {
+            statistics.put(term, statistics.get(term) + 1);
+        } else {
+            statistics.put(term, 1);
         }
     }
 
     private static void exit() {
         System.out.println("Bye bye!");
         System.exit(0);
+    }
+
+    private static void log() {
+        System.out.println("File name:");
+        String fileName = scanner.nextLine();
+
+        File file = new File(fileName);
+
+        try (PrintWriter writer = new PrintWriter(file)) {
+            /*
+            for (var entry : data.entrySet()) {
+                writer.printf("%s:%d%n", entry.getKey(), statistics.getOrDefault(entry.getKey(), 0));
+            }
+            */
+            for (int i = 0; i < 33; i++) {
+                writer.println("Hey");
+            }
+        } catch (IOException e) {
+            System.out.printf("An exception occurs %s", e.getMessage());
+        }
+        System.out.println("The log has been saved.");
+    }
+
+    private static void hardestCard() {
+        if (statistics.isEmpty()) {
+            System.out.println("There are no cards with errors.");
+        } else {
+            Map<Integer, List<Map.Entry<String, Integer>>> groupedStatistics = statistics.entrySet().stream().collect(Collectors.groupingBy(Map.Entry::getValue));
+            Optional<List<Map.Entry<String, Integer>>> hardestGroup = groupedStatistics.entrySet().stream().max(Map.Entry.comparingByKey()).map(Map.Entry::getValue);
+            if (hardestGroup.isPresent()) {
+                List<Map.Entry<String, Integer>> hardestCards = hardestGroup.get();
+                if (hardestCards.size() > 1) {
+                    List<String> hardestCardsTerms = hardestCards.stream().map(Map.Entry::getKey).collect(Collectors.toList());
+                    System.out.print("The hardest cards are ");
+                    for (int i = 0; i < hardestCardsTerms.size() - 1; i++) {
+                        System.out.printf("\"%s\", ", hardestCardsTerms.get(i));
+                    }
+                    System.out.printf("\"%s\". You have %d errors answering them.%n", hardestCardsTerms.get(hardestCards.size() - 1), hardestCards.get(0).getValue());
+                } else {
+                    Map.Entry<String, Integer> hardestCard = hardestCards.get(0);
+                    System.out.printf("The hardest card is \"%s\". You have %d errors answering it.%n", hardestCard.getKey(), hardestCard.getValue());
+                }
+            }
+        }
+    }
+
+    private static void resetStats() {
+        statistics.clear();
+        System.out.println("Card statistics have been reset.");
     }
 }
